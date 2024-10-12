4108fc36060fc9e38dae95f2d7b64593e116a23d
“ilyag26” <gayvoronskiy.i02@gmail.com>
2024-10-12 23:15:57 +0200

    changed logic

--- src/Field.jsx
+++ src/Field.jsx
@@ -1,15 +1,8 @@
 import { useState } from 'react';
 
-function Table(){
-  
-  const [value, setValue] = useState(null);
-  
-  function makeStep(){
-    setValue('X')
-  }
-
+function Table({value, clickAction}){
   return (
-    <button className="square" onClick={makeStep}>
+    <button className="square" onClick={clickAction}>
       {value}
     </button>
   );
@@ -17,30 +10,37 @@ function Table(){
 }
 
 function Field(){
+  const [arrayField, changeField] = useState(Array(9).fill(null))
 
-return(
-  <>
-    <div className="card">
-      <h2 className="title-card">TicTacToe</h2>
-        <div className="field">
-          <div className="row">
-          <Table />
-          <Table />
-          <Table />
+  function clickAction(i){
+    const nextSquares = arrayField.slice();
+    nextSquares[i] = 'X';
+    changeField(nextSquares);
+  }
+
+  return(
+    <>
+      <div className="card">
+        <h2 className="title-card">TicTacToe</h2>
+          <div className="field">
+            <div className="row">
+            <Table value={arrayField[0]} clickAction={() => clickAction(0)} />
+            <Table value={arrayField[1]} clickAction={() => clickAction(1)}/>
+            <Table value={arrayField[2]} clickAction={() => clickAction(2)}/>
+            </div>
+            <div className="row">
+            <Table value={arrayField[3]} clickAction={() => clickAction(3)}/>
+            <Table value={arrayField[4]} clickAction={() => clickAction(4)}/>
+            <Table value={arrayField[5]} clickAction={() => clickAction(5)}/>
+            </div>
+            <div className="row">
+            <Table value={arrayField[6]} clickAction={() => clickAction(6)}/>
+            <Table value={arrayField[7]} clickAction={() => clickAction(7)}/>
+            <Table value={arrayField[8]} clickAction={() => clickAction(8)}/>
+            </div>
           </div>
-          <div className="row">
-          <Table />
-          <Table />
-          <Table />
-          </div>
-          <div className="row">
-          <Table />
-          <Table />
-          <Table />
-          </div>
-        </div>
-    </div>
-  </>
+      </div>
+    </>
 );
 
 
